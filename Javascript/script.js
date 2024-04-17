afd94e865dae9e09ddb26ba7a5c1c1864cb4aa2a
maily-vannobel <maily.vannobel@laplateforme.io>
2024-04-17 23:45:24 +0200

    ajout 2e monnaie + fonction auto element de la boutique + menu des bonus

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,24 +1,39 @@
-// SELECTIONNER PERSO + PORTE MONNAIE + L'INITALISE A 0
 const arbreCliquable = document.querySelector('.arbrePrincipal');
+       //ARGENT
 const portemonnaie = document.querySelector('.portemonnaie')
+const portebillets = document.querySelector('.portebillets')
 let points = 0;
+let billets = 0;
+
+      //EASTER EGG
+const bonusTerre = document.querySelector('.bonus1')
+let intervalId;
+let tauxGenerationAuto;
 
     function mettreAJourPorteMonnaie() {
-    portemonnaie.textContent = `Porte-monnaie : ${points}`;
+    portemonnaie.textContent = `🪙 : ${points}`;
     }
+
+    function mettreAJourPorteBillets() {
+      portebillets.textContent = `💵 : ${billets}`;
+      }
+
+
+  //SCRIPT evenement clics
+arbreCliquable.addEventListener('click', gererClicArbre);
+bonusTerre.addEventListener('click', gererClicBonusTerre);
 
 
 // FONCTIONS AU CLIC
-    function gererClicArbre() { 
-        points++;
-        mettreAJourPorteMonnaie();
-    }
-    
-    //ajouter clic sur l'arbre + incrémenter points
-    arbreCliquable.addEventListener ('click', () => {
-});
+function gererClicArbre() { 
+  points++;
+  mettreAJourPorteMonnaie();
+}
 
-
+function gererClicBonusTerre() {
+  billets++;
+  mettreAJourPorteBillets();
+}
 
 
 // ----- FONCTIONS AUTOMATIQUES
@@ -26,4 +41,55 @@ let points = 0;
       points++;
       mettreAJourPorteMonnaie(); 
 }
-    setInterval(ajouterPointsAutomatiquement, 5000);
+   //déclare 
+ setInterval(ajouterPointsAutomatiquement, 5000);
+
+//ACHAT DE POUVOIRS AUTO
+function acheterPouvoirAutomatique(cout,tempsEntrePoints,pointsParGeneration){
+  if(points >= cout){  
+    points -= cout; 
+    mettreAJourPorteMonnaie();  
+    tauxGenerationAuto = pointsParGeneration;
+    // Déclenche génération auto de pts en fonction du tps entre chaque génération
+     if (!intervalId) {
+      intervalId = setInterval(function() {
+        points += pointsParGeneration;
+        mettreAJourPorteMonnaie();
+    }, tempsEntrePoints * 1000); // Convertit secondes en millisecondes
+}
+    afficherLicornePopover("Pouvoir acheté !");
+} else {
+    afficherLicornePopover("Pas assez de sous frero  ");
+  }}
+
+
+
+
+
+// BULLE DE DIALOGUE
+      function afficherLicornePopover(message) {
+        const licornePopover = document.getElementById('licornePopover');
+        const licorneMessage = document.getElementById('licorneMessage');
+
+        licorneMessage.textContent = message;
+        licornePopover.classList.remove('d-none');
+
+        // Masquer la bulle de dialogue après quelques secondes
+        setTimeout(function() {
+          licornePopover.classList.add('d-none');
+        }, 5000); // Durée en millisecondes (5 secondes dans cet exemple)
+      }
+
+      // Fonction pour fermer la bulle de dialogue de la licorne
+      function fermerLicornePopover() {
+        const licornePopover = document.getElementById('licornePopover');
+        licornePopover.classList.add('d-none');
+      }
+
+//-------  LOCAL STORAGE ----------
+
+// DESCRIPTION POUVOIR AU SURVOL  
+  var popovers = document.querySelectorAll('[data-bs-toggle="popover"]');
+  popovers.forEach(function (popover) {
+    new bootstrap.Popover(popover);
+  });
